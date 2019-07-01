0ab8e3718baeb5ab990d0bbbce30ba7dd702d532
mahoncj <cmahon90@gmail.com>
2019-07-01 19:01:16 -0400

    Update azurestackmarketplace.ps1
    Prompts user for ARM Admin URI and AAD Tenant Name. Adds/Removes Microsoft extensions only. Adds logic to compare available extensions with currently installed extensions. Removes only older verisons of Microsoft extensions.

--- azurestackmarketplace.ps1
+++ azurestackmarketplace.ps1
@@ -1,78 +1,81 @@
-#Update the two below vaules for your Azure Stack Environment
-$ArmEndpoint = "https://adminmanagement.<dns zone>/"
-$StackTenantID = "<GUID>"
+# Get ARM Admin Endpoint URI and AAD Tenant Name
+Write-Host "Getting ARM Admin Endpoint" -ForegroundColor Green
+$armEndpoint = Read-Host "Please enter your ARM Admin Endpoint URI (ex: https://adminmanagement.region.contoso.com)"
+Write-Host ""
+Write-Host "Getting AAD Tenant Name" -ForegroundColor Green
+$AADTenantName = Read-Host "Please enter your AAD Tenant Name (ex: contoso.onmicrosoft.com)"
+Write-Host ""
 
+# Authenticate to the Azure Stack Environment with an account that has access to the Default Provider Subscription
+Add-AzureRMEnvironment -Name "AzureStackAdmin" -ArmEndpoint $armEndpoint -ErrorAction Stop
+$AuthEndpoint = (Get-AzureRmEnvironment -Name "AzureStackAdmin").ActiveDirectoryAuthority.TrimEnd('/')
+$TenantId = (invoke-restmethod "$($AuthEndpoint)/$($AADTenantName)/.well-known/openid-configuration").issuer.TrimEnd('/').Split('/')[-1]
+Add-AzureRmAccount -EnvironmentName "AzureStackAdmin" -TenantId $TenantId
 
-
-$cred = Get-Credential
-
-Add-AzureRMEnvironment -Name "AzureStackAdmin" -ArmEndpoint $ArmEndpoint -ErrorAction Stop 
-Login-AzureRmAccount -Environment "AzureStackAdmin" -Credential $cred -TenantId $StackTenantID #as someone with access to the default subscription
-
+# Verify Azure Stack is registered and activation resource group exists
 $activationRG = "azurestack-activation"
-$bridgeactivation = Get-AzsAzureBridgeActivation -ResourceGroupName $activationRG 
-$activationName = $bridgeactivation.Name
+$bridgeActivation = Get-AzsAzureBridgeActivation -ResourceGroupName $activationRG
+$activationName = $bridgeActivation.Name
 
-#Want all the extensions
-$getExtensions = ((Get-AzsAzureBridgeProduct -ActivationName $activationName -ResourceGroupName $activationRG -ErrorAction SilentlyContinue -Verbose | Where-Object {($_.ProductKind -eq "virtualMachineExtension") -and ($_.Name -like "*microsoft*")}).Name) -replace "default/", ""
+# Get available Microsoft extensions and compare with extensions already downloaded to Azure Stack marketplace
+$availableExtensions = ((Get-AzsAzureBridgeProduct -ActivationName $activationName -ResourceGroupName $activationRG -ErrorAction SilentlyContinue | Where-Object {($_.ProductKind -eq "virtualMachineExtension") -and ($_.Name -like "*microsoft*")}).Name) -replace "default/", ""
+$myExtensions = ((Get-AzsAzureBridgeDownloadedProduct -ActivationName $activationName -ResourceGroupName $activationRG -ErrorAction SilentlyContinue | Where-Object {($_.ProductKind -eq "virtualMachineExtension") -and ($_.Name -like "*microsoft*")}).Name) -replace "default/", ""
+$diffExtensions = Compare-Object $myExtensions $availableExtensions 
+$missingExtensions = $diffExtensions | Where-Object {$_.SideIndicator -eq "=>"}
+$missingExtensions = $missingExtensions.InputObject
 
-foreach ($extension in $getExtensions) 
+# Print all missing Microsoft extensions in window
+Write-Host "The following Microsoft extensions have newer versions:"
+$missingExtensions | Write-Host -ForegroundColor Red
+Write-Host ""
+
+# Allow user to choose whether or not to download the missing Microsoft extensions
+$prompt = Read-Host "Do you want to download the missing extensions to the marketplace? (y/n)"
+Switch ($prompt)
 {
-    Write-Output "Checking for $extension"
-    if (!$(Get-AzsAzureBridgeDownloadedProduct -Name $extension -ActivationName $activationName -ResourceGroupName $activationRG -ErrorAction SilentlyContinue))
-    { 
-        Write-Output "** Didn't find $extension in your gallery. Downloading from the Azure Stack Marketplace **"
-        Invoke-AzsAzureBridgeProductDownload -ActivationName $activationName -Name $extension -ResourceGroupName $activationRG -Force -Confirm:$false
+    Y
+    {
+        foreach ($missingExtension in $missingExtensions)
+        {
+            Write-Host "Downloading $missingExtension to the Azure Stack Marketplace" -ForegroundColor Green
+            #Start-Sleep -Seconds 5
+            Invoke-AzsAzureBridgeProductDownload -ActivationName $activationName -Name $missingExtension -ResourceGroupName $activationRG -Force -Confirm:$false
+        }
+    }
+    N
+    {
+        Write-Host "No extensions were downloaded!" -ForegroundColor Red
+        Write-Host ""
+    }
+    Default
+    {
+        Write-Host "No extensions were downloaded!" -ForegroundColor Red
+        Write-Host ""
     }
 }
-
-#Get core images   PublisherDisplayName, PublisherIdentifier, Offer, Sku
-$imagesrequired = @(@("Canonical","Canonical","UbuntuServer","16.04-LTS"),
-                    @("Canonical","Canonical","UbuntuServer","18.04-LTS"),
-                    @("Microsoft","MicrosoftWindowsServer","WindowsServer","2016-Datacenter"),
-                    @("Microsoft","MicrosoftWindowsServer","WindowsServer","2016-Datacenter-Server-Core"),
-                    @("Microsoft","MicrosoftWindowsServer","WindowsServer","2016-Datacenter-with-Containers"))
-
-$getAllImages = Get-AzsAzureBridgeProduct -ActivationName $activationName -ResourceGroupName $activationRG | Where-Object {($_.ProductKind -eq "virtualMachine")}
-foreach ($image in $imagesrequired)
-{
-    Write-Output "Checking for $($image[2])-$($image[3])"
-    #Need to check the name of the latest
-    $templist = $getAllImages | Where-Object {$_.PublisherDisplayName -eq $image[0] -and $_.Offer -eq $image[2] -and $_.Sku -eq $image[3] -and $_.DisplayName -notlike "*Pay as you*"}
-    $templist = $templist | Sort-Object -Property ProductProperties -Descending
-    $imagename = $templist[0].Name -replace "default/", ""  #is the latest
-    #Write-Output "Checking for name $imagename"
-    if (!$(Get-AzsAzureBridgeDownloadedProduct -ActivationName $activationName -ResourceGroupName $activationRG -Name $imagename -ErrorAction SilentlyContinue))
-    { 
-        Write-Output "** Didn't find image in your gallery. Downloading from the Azure Stack Marketplace **"
-        Invoke-AzsAzureBridgeProductDownload -ActivationName $activationName -Name $imagename -ResourceGroupName $activationRG -Force -Confirm:$false
-    }
-}
-
-#Now check for multiple versions of something and clean the old
 
 #Get what is installed
-$allinstalled = Get-AzsAzureBridgeDownloadedProduct -ActivationName $activationName -ResourceGroupName $activationRG
-$allinstalled = $allinstalled | Sort-Object -Property DisplayName, ProductProperties -Descending #want newest first as we'll look for matching and remove the second
-#$allinstalled | fl displayname, offerversion, ProductProperties, PublisherDisplayName, PublisherIdentifier, Offer, SKU, Name
+$installedExtensions = Get-AzsAzureBridgeDownloadedProduct -ActivationName $activationName -ResourceGroupName $activationRG | Where-Object {($_.ProductKind -eq "virtualMachineExtension") -and ($_.Name -like "*microsoft*")}
+$installedExtensions = $installedExtensions | Sort-Object -Property DisplayName, ProductProperties -Descending #want newest first as we'll look for matching and remove the second
 
 $prevDisplayName = "Not going to match"
 $prevEntry = $null
-foreach($installed in $allinstalled)
+foreach($installed in $installedExtensions)
 {
-    #see if name matches the previous, i.e. same image
+    #see if name matches the previous, i.e. same extension
     if($installed.DisplayName -eq $prevDisplayName)
     {
         #Lets remove it 
-        Write-Output "** Found an older version of $($installed.DisplayName) **"
-        Write-Output "   Previous version is $($installed.ProductProperties.Version) - $($installed.Name)"
-        Write-Output "   Current version is $($prevEntry.ProductProperties.Version) - $($prevEntry.Name)"
+        Write-Host "** Found an older version of $($installed.DisplayName) **"
+        Write-Host "Previous version is $($installed.ProductProperties.Version) - $($installed.Name)" -ForegroundColor Red
+        Write-Host "Current version is $($prevEntry.ProductProperties.Version) - $($prevEntry.Name)" -ForegroundColor Green
+        Write-Host ""
         $Readhost = Read-Host " Do you want to delete previous version ($($installed.ProductProperties.Version)) (y/n)?"
         Switch ($ReadHost) 
          { 
-           Y {Write-host " Yes, Removing old version"; Remove-AzsAzureBridgeDownloadedProduct -Name $installed.Name -ActivationName $activationName -ResourceGroupName $activationRG -Force -Confirm:$false -ErrorAction Continue} 
-           N {Write-Host " No, Not removing"} 
-           Default {Write-Host " Default, Not removing"} 
+           Y {Write-host " Yes, removing older extension version"; Remove-AzsAzureBridgeDownloadedProduct -Name $installed.Name -ActivationName $activationName -ResourceGroupName $activationRG -Force -Confirm:$false -ErrorAction Continue} 
+           N {Write-Host " No, not removing older extension version"} 
+           Default {Write-Host " No, not removing older extension version"} 
          }
 
         Write-Output ""
